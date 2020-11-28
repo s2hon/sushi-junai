a207c2d2c8a639d03ac7fefc17717bb02afe35bc
Michelle Moon <robbins.michelle@ymail.com>
2020-11-28 16:29:55 -0600

    image component and beginning Home oage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,40 @@ import Container from "../components/Container";
 import Row from "../components/Row";
 import Footer from "../components/Footer";
 import Counter from "../components/Counter";
+import Col from "../components/Col";
+import Image from "../components/Image";
 
 function Home(props) {
     return (<>
     <Nav/>
         <Container>
             <Row>
-                <Counter></Counter>
-                <Card />
+                <Col size={"sm"} number={6}>
+                <Card><ul>
+                    <li>Location 1</li>
+                    <li>Phone number</li>
+                    <li>Address</li>
+                    </ul>
+                    </Card>
+                </Col>
+                <Col size={"sm"} number={6}>
+                <Card>
+                <ul>
+                    <li>Location 2</li>
+                    <li>Phone number</li>
+                    <li>Address</li>
+                    </ul>
+                </Card>
+                </Col>
+                </Row>
+                <Row>
+                    <Col size={"md"} number={12}>
+                <Image src={"./assests/logo.png"} alt={"fish logo"}/>
+                </Col>
             </Row>
+            <Col size={"md"} number={6}>
+                {/* button component */}
+            </Col>
         </Container>
         <Footer />
     </>);
