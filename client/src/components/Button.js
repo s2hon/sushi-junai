92d3696790151dd857afd50a0b371d5ed722aa86
Michelle Moon <robbins.michelle@ymail.com>
2020-11-30 13:29:07 -0600

    fixed typo on button

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function Button(props) {
     return (
-        <button type={`${props.type}`} className={`${props.btn}`}><i className={`${props.icon}`} onClick={`${props.function}`}></i>{props.children}</button>
+        <button type={`${props.type}`} className={`${props.btn}`} onClick={`${props.function}`}><i className={`${props.icon}`}></i>{props.children}</button>
     );
 }
 
