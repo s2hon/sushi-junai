ff669a98c994d2ae2ec4270823a8d8e200d62065
Michelle Moon <robbins.michelle@ymail.com>
2020-11-30 18:26:24 -0600

    working timer component

--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -1,25 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 function Counter() {
-    const [count, setCount] = useState(2 * 60 * 60);
+    const [count, setCount] = useState(7199);
     const [hour, setHour] = useState(0);
     const [minute, setMinute] = useState(0);
     const [seconds, setSeconds] = useState(0);
-
     
-        setTimeout(() => {
-            setCount(count - 1)
-            setHour(Math.floor(count/ 7200))
-            setMinute(Math.floor(count/ 60000))
-            setSeconds(Math.floor(minute / 1000))
+    useEffect(() => { 
+        const timer = setTimeout(() => {
+            setCount(count - 1);
+            setHour(Math.floor(count/ 3600));
+            setMinute(Math.floor(Math.floor((count) - (Math.floor(count/ 3600)*3600)) / 60));
+            setSeconds(count % 60);
         }, 1000)
-        if (count===0){
-            clearTimeout();
-        }
+        
+            if (count< 0){
+                clearTimeout(timer);
+            }
+    }, [count])
     
     return (
-        <div>Time:{count}
+        <div>{hour}:{minute}:{seconds}
             </div>
     )
 }
