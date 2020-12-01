e35c911afa02fa3287c8557cc316a54aae37b041
Michelle Moon <robbins.michelle@ymail.com>
2020-12-01 17:41:30 -0600

    check button

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,24 +1,24 @@
 import React from "react";
 import Card from "../components/Card";
-import Button from "../components/Button";
+ import Button from "../components/Button";
 
 function Slider(props){
   return(<>
-  <Card>
+ <Card>
     <h3>Absolutely NO SHARING between All you can eat and A LA CARTE customers</h3>
-    <Button icon ={"fa fa-chevron-circle-right fa-3x"}></Button>
+    <Button icon ={"fa fa-check fa-3x"}></Button>
   </Card>
   <Card>
     <h3>If ALL items ordered are not completely consumed, you may be charged A LA CARTE pricing</h3>
-    <Button icon ={"fa fa-chevron-circle-right fa-3x"}></Button>
+    <Button icon ={"fa fa-check fa-3x"}></Button>
     </Card>
   <Card>
     <h3>Items marked with red text can only be ordered once</h3>
-    <Button icon ={"fa fa-chevron-circle-right fa-3x"}></Button>
+    <Button icon ={"fa fa-check fa-3x"}></Button>
     </Card>
   <Card><h3>Ordering time is limited to 2 Hours</h3>
-  <Button icon ={"fa fa-chevron-circle-right fa-3x"}></Button>
-  </Card>
+  <Button icon ={"fa fa-check fa-3x"}></Button>
+  </Card> 
   </>
   
   );
