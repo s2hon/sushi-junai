1255642e7694ddc23313a1f78061dfe09f7c4d5e
Michelle Moon <robbins.michelle@ymail.com>
2020-12-01 17:47:12 -0600

    route to behing AYCE order

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,26 +1,29 @@
 import React from "react";
 import Card from "../components/Card";
- import Button from "../components/Button";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
 
-function Slider(props){
-  return(<>
- <Card>
-    <h3>Absolutely NO SHARING between All you can eat and A LA CARTE customers</h3>
-    <Button icon ={"fa fa-check fa-3x"}></Button>
-  </Card>
-  <Card>
-    <h3>If ALL items ordered are not completely consumed, you may be charged A LA CARTE pricing</h3>
-    <Button icon ={"fa fa-check fa-3x"}></Button>
+function Slider(props) {
+  return (<>
+    <Card>
+      <h3>Absolutely NO SHARING between All you can eat and A LA CARTE customers</h3>
+      <Button icon={"fa fa-check fa-3x"}></Button>
     </Card>
-  <Card>
-    <h3>Items marked with red text can only be ordered once</h3>
-    <Button icon ={"fa fa-check fa-3x"}></Button>
+    <Card>
+      <h3>If ALL items ordered are not completely consumed, you may be charged A LA CARTE pricing</h3>
+      <Button icon={"fa fa-check fa-3x"}></Button>
     </Card>
-  <Card><h3>Ordering time is limited to 2 Hours</h3>
-  <Button icon ={"fa fa-check fa-3x"}></Button>
-  </Card> 
+    <Card>
+      <h3>Items marked with red text can only be ordered once</h3>
+      <Button icon={"fa fa-check fa-3x"}></Button>
+    </Card>
+    <Card><h3>Ordering time is limited to 2 Hours</h3>
+      <Link to={"/menu"}>
+        <Button icon={"fa fa-check fa-3x"} ></Button>
+      </Link>
+    </Card>
   </>
-  
+
   );
 }
 
