a63d17651e21a292f633ebea36a91275b528c8ea
Christi Kerr <christinakerr394@gmail.com>
2020-12-02 14:51:41 -0600

    Rezising image in home.js

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -2,7 +2,7 @@ import React from "react";
     
 function Image(props) {
     return (
-        <img src={`${props.src}`} alt={`${props.alt}`}/>
+        <img src={`${props.src}`} width={props.width} alt={`${props.alt}`}/>
     );
 }
 
