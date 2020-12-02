74c5437f18eea303ae4bc95748bf99d5814df53d
Christi Kerr <christinakerr394@gmail.com>
2020-12-02 14:35:39 -0600

    Resized homepage image to fit inside its container

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function Image(props) {
     return (
-        <img src={`${props.src}`} alt={`${props.alt}`} />
+        <img width="100%" src={`${props.src}`} alt={`${props.alt}`} />
     );
 }
 
