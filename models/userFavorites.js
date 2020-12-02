c7260f245df60ad2b49490424cda0eb8de16e552
aminadab.morales <aminadabm93@gmail.com>
2020-12-02 17:46:11 -0600

    created favorites model

---
+++ models/userFavorites.js
@@ -0,0 +1,23 @@
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
+// var bcrypt = require("bcryptjs");
+// Creating our User model
+module.exports = function(sequelize, DataTypes) {
+  var UserFavorites = sequelize.define("UserFavorites", {
+    // The email cannot be null, and must be a proper email before creation
+    item: {
+      type: DataTypes.STRING,
+      unique: true      
+    }
+  });
+
+
+  //here we will add a user.associate to connect to  favorites model 
+  UserFavorites.associate = function(models) {
+    // Associating user with favorites
+    // When an favorite is deleted. we will have to update the array 
+    UserFavorites.belongsTo(models.User, {
+    //   onDelete: "cascade"
+    });
+  };
+  return UserFavorites;
+};
