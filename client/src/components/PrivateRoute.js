95c075da5ab58679c5a44d8626cb233b7410f8a8
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-12-02 17:09:00 -0600

    small changes to location in private route

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -5,10 +5,11 @@ import {Route, useLocation, Redirect} from "react-router-dom";
 function PrivateRoute({ children , ...rest}) {
     const [state, dispatch] = useStoreContext();
     const location = useLocation();
+    const {from} = location.state || { from: {pathname: "/"}}
 
     return (
     <Route {...rest}
-    render={({location})=>state.isLoggedIn ? (children) : 
+    render={(location.state)=>state.isLoggedIn ? (children) : 
     (<Redirect to={{pathname: "/login",state: {from: location}}}/>)}/>
     )
 }
