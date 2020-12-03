2c65da958e97ab394b0d9745c39b13ef28368ef7
Staci Shon <staci.shon@gmail.com>
2020-12-02 19:33:57 -0600

    resolved conflict

--- client/src/Router/index.js
+++ client/src/Router/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Switch, Route } from 'react-router-dom'
-
-import Home from "../pages/Home";
-import Alacarte from "../pages/Alacarte";
-import VisibleItemList from "../pages/Ayce";
-import Favorite from "../pages/Favorite";
-import VisibleCart from "../pages/OrderSummary";
-import Signup from "../pages/Signup";
-import Choice from "../pages/Choice";
-import Rules from "../pages/Rules";
-import Login from "../pages/Login"; 
+import Home from "./pages/Home";
+import Alacarte from "./pages/Alacarte";
+import AYCEMenu from "./pages/AYCEMenu";
+import Favorite from "./pages/Favorite";
+import OrderSummary from "./pages/OrderSummary";
+import Signup from "./pages/Signup";
+import Choice from "./pages/Choice";
+import Rules from "./pages/Rules";
+import Login from "./pages/Login";
 
 const AllRoute = () => { 
 
@@ -19,12 +18,13 @@ const AllRoute = () => {
         <Route exact path="/" component={Home}/>
         <Route exact path="/home" component={Home}/>
         <Route exact path="/alacarte" component={Alacarte}/>
-        <Route exact path="/ayce" component={VisibleItemList}/>
-        <Route exact path="/menu" component={Choice}/>
+        <Route exact path="/menu" component={AYCEMenu}/>
+        <Route exact path="/choice" component={Choice}/>
         <Route exact path="/favorite" component={Favorite}/>
-        <Route exact path="/ordersummary" component={VisibleCart}/> 
-        <Route exact path="/SignUp" component={Signup}/>
+        <Route exact path="/ordersummary" component={OrderSummary}/> 
+        <Route exact path="/Signup" component={Signup}/>            
         <Route exact path="/rules" component={Rules}/>
+        <Route exact path="/login" component={Login}/>
       </Switch>
     </div>
   );
