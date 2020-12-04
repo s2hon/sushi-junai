de7b0e18cde928203f634f573ba7e85784a216c8
Staci Shon <staci.shon@gmail.com>
2020-12-04 17:36:20 -0600

    delete

--- client/src/reducer/menuCart.js
+++ client/src/reducer/menuCart.js
@@ -11,6 +11,18 @@ const menuCart = (state = persistedState || [], action) => {
                 price: item.price,
                 category: item.category
             }];
+        case 'CHANGE_ITEM':
+            const { item } = action.payload
+            return [...state, {
+                name: item.name,
+                category: item.category
+            }];
+        case 'SUB_FAV_ITEM':
+        const { item } = action.payload
+        return [...state, {
+            name: item.name,
+            category: item.category
+        }];
         default:
             return state
     }
