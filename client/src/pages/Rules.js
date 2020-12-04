b307661bdd8dda2024fdbcb382384f655500f9c6
Michelle Moon <robbins.michelle@ymail.com>
2020-12-04 10:59:09 -0600

    putting the counter back on page

--- client/src/pages/Rules.js
+++ client/src/pages/Rules.js
@@ -1,12 +1,8 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 import Container from "../components/Container";
-// import Row from "../components/Row";
 import Footer from "../components/Footer/index";
 import Slider from "../components/Slider";
-// import Col from "../components/Col";
-// import Image from "../components/Image";
-// import Button from "../components/Button";
+
 
 function Rules(props) {
     return (
