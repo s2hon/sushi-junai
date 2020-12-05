9bc17d0b23979601e18e79f8d2730901c246892f
Christi Kerr <christinakerr394@gmail.com>
2020-12-05 15:06:33 -0600

    Added initial lunch menu files

---
+++ client/src/pages/LunchMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LunchMenu from "../components/LunchMenu/MenuList"
+
+
+function LunchMenu() {
+    return (
+        <>
+        <LunchMenu />
+        </>
+        );
+}
+
+export default LunchMenu;
