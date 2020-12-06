58c0b1b755250f56858eaff7152d4a8dd9701d5d
Christi Kerr <christinakerr394@gmail.com>
2020-12-06 14:11:58 -0600

    Changed color of menu dropdown options

--- client/src/components/MenuNav/index.js
+++ client/src/components/MenuNav/index.js
@@ -9,26 +9,26 @@ const NavBar = () => {
     return (
         <>
         <Nav tabs>
-            <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+            <Dropdown  nav isOpen={dropdownOpen} toggle={toggle}>
                 <DropdownToggle nav caret>
                     Menu
                 </DropdownToggle>
                 <DropdownMenu>
-                    <DropdownItem active><a href="#appetizers">Appetizers</a></DropdownItem>
-                    <DropdownItem active><a href="#salad">Salads</a></DropdownItem>
-                    <DropdownItem active><a href="#soup">Soup/Noodles</a></DropdownItem>
-                    <DropdownItem active><a href="#rice">Rice</a></DropdownItem>
+                    <DropdownItem><a href="#appetizers">Appetizers</a></DropdownItem>
+                    <DropdownItem><a href="#salad">Salads</a></DropdownItem>
+                    <DropdownItem><a href="#soup">Soup/Noodles</a></DropdownItem>
+                    <DropdownItem><a href="#rice">Rice</a></DropdownItem>
                     <DropdownItem divider />
-                    <DropdownItem active><a href="#entree">Entrees</a></DropdownItem>
-                    <DropdownItem active><a href="#sushiSashimi">Sushi & Sashimi</a></DropdownItem>
-                    <DropdownItem active><a href="#platters">Sashimi Platters</a></DropdownItem>
-                    <DropdownItem active><a href="#classicRoll">Classic Rolls/Hand Rolls</a></DropdownItem> 
-                    <DropdownItem active><a href="#sushi">Sushi(Nigiri)</a></DropdownItem> 
-                    <DropdownItem active><a href="#chefsSpecial">Chef's Special Rolls</a></DropdownItem> 
-                    <DropdownItem active><a href="#tempura">Tempura Rolls</a></DropdownItem> 
-                    <DropdownItem active><a href="#bakedRolls">Baked Rolls</a></DropdownItem> 
+                    <DropdownItem><a href="#entree">Entrees</a></DropdownItem>
+                    <DropdownItem><a href="#sushiSashimi">Sushi & Sashimi</a></DropdownItem>
+                    <DropdownItem><a href="#platters">Sashimi Platters</a></DropdownItem>
+                    <DropdownItem><a href="#classicRoll">Classic Rolls/Hand Rolls</a></DropdownItem> 
+                    <DropdownItem><a href="#sushi">Sushi (Nigiri)</a></DropdownItem> 
+                    <DropdownItem><a href="#chefsSpecial">Chef's Special Rolls</a></DropdownItem> 
+                    <DropdownItem><a href="#tempura">Tempura Rolls</a></DropdownItem> 
+                    <DropdownItem><a href="#bakedRolls">Baked Rolls</a></DropdownItem> 
                     <DropdownItem divider />
-                    <DropdownItem active><a href="#desserts">Desserts</a></DropdownItem> 
+                    <DropdownItem><a href="#desserts">Desserts</a></DropdownItem> 
                 </DropdownMenu>
             </Dropdown>
         </Nav>
