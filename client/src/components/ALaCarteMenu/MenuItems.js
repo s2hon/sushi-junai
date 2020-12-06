0871ff8412d7256c2bbe37ce52980d5329b01eb9
Christi Kerr <christinakerr394@gmail.com>
2020-12-06 11:59:34 -0600

    Built a la carte menu page

---
+++ client/src/components/ALaCarteMenu/MenuItems.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "./Image";
+import fishIcon from "../AyceMenu/icons8-fish-food-50.png"
+import vegIcon from "../AyceMenu/icons8-vegetarian-mark-50.png"
+import gfIcon from "../AyceMenu/icons8-no-gluten-50.png"
+import { Col } from 'reactstrap';
+
+function MenuItems(props) {
+        return (
+                <>
+                <Col sm="1" md="3" lg="3">
+                        <h5>{props.name} <br/> 
+                        
+                        {props.fish ? 
+                        <a href="ayce#rawfish"><Image src={fishIcon} width="18" alt="Fish" /></a>
+                        : <span></span>}
+                        {
+                                props.vegetarian ?
+                                <a href="ayce#vegetarian"><Image src={vegIcon} width="18" alt="Vegetarian" /></a>
+                                : <span></span>
+                        }
+                        {
+                                props.glutenFree ?
+                                <a href="ayce#glutenFree"><Image src={gfIcon} width="18" alt="Gluten Free" /></a>
+                                : <span></span>
+                        }
+                        </h5>
+                        $ {props.price.toFixed(2)}
+                </Col>
+                <Col sm="auto">
+                        <p>{props.description}
+                        
+                        </p>
+                </Col>
+                {props.children}
+                </>
+        )
+}
+
+export default MenuItems;
