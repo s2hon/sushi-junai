091805bc9e292953a1f0d0be05859ef7f04ce0d2
Michelle Moon <robbins.michelle@ymail.com>
2020-12-07 10:31:36 -0600

    conflicts

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './style.css';
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-// import { 
-//   faFacebookF,
-//   faInstagram
-// } from "@fortawesome/free-brands-svg-icons"; 
+import { 
+  faFacebookF,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons"; 
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = (props) =>{
@@ -18,8 +18,8 @@ const Footer = (props) =>{
               <div className="widget about-widget">
                 <p>Connect with us!</p>
                 <ul className="text-center">
-                    {/* <li><a target={"_blank"} rel="noreferrer" href="https://www.facebook.com/sushijunai/"><FontAwesomeIcon icon={faFacebookF} size="2x"/></a></li>
-                    <li><a target={"_blank"} rel="noreferrer" href="https://www.instagram.com/sushijunai/"><FontAwesomeIcon icon={faInstagram} size="2x"/></a></li> */}
+                    <li><a target={"_blank"} rel="noreferrer" href="https://www.facebook.com/sushijunai/"><FontAwesomeIcon icon={faFacebookF} size="2x"/></a></li>
+                    <li><a target={"_blank"} rel="noreferrer" href="https://www.instagram.com/sushijunai/"><FontAwesomeIcon icon={faInstagram} size="2x"/></a></li>
                     <li><a target={"_blank"} rel="noreferrer" href="mailto:sushijunai1@gmail.com"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
                 </ul>
               </div>
