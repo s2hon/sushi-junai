6177228482724ea0f1e4af8d8b3805b48b328fc2
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-12-06 20:15:54 -0600

    restructured home page and added a carousel of menu items

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,15 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Card from "../components/Card/index";
 import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
-import Image from "../components/Image";
+import {Row, Col} from "reactstrap";
 import Button from "../components/Button";
 import data from "../db/store.json";
 import { useStoreContext } from '../utils/GlobalStore';
+import HomeCarousel from "../components/HomeCarousel"
 
 function Home(props) {
-    const [state, dispatch] = useStoreContext();
+    const [state] = useStoreContext();
 
     return (
     <React.Fragment>
@@ -19,33 +18,38 @@ function Home(props) {
                 {
                     data.map((data, idx) => {
                         return (
-                            <Col size={"sm"} number={6}>
+                
                                 <Card key={idx} location={data.location} phone={data.phone} address={data.address}
                                 mon={data.mon} tue={data.tue} wed={data.tue} thu={data.thu} fri={data.fri} sat={data.sat} sun={data.sun}>
                                 </Card>
-                            </Col>
+                        
                         )
                     })
                 }
             </Row>
-            <Row>
-                <Image src={"./assets/rest-img.JPG"} alt={"picture of sushi with drinks in background"} width="100%" />
+            <Row >
+                <Col sm="12" md={{ size: 6, offset: 3 }} lg={{size: 6, offset: 3}}>
+                <HomeCarousel />
+                </Col>
             </Row>
-            <Col size={"md"} number={6}>
-                <Row><a href={"https://www.yelp.com/biz/sushi-junai-austin"}>
+            <Row>
+                <Col>
+                <a href={"https://www.yelp.com/biz/sushi-junai-austin"}>
                     <Button type={"button"} btn={"btn btn-dark"}>Reservations</Button>
                 </a>
-                </Row>
-                <Row>
-                    <Link to={!state.userLoggedIn ? "/login": "/choice"}>
-                        <Button type={"button"} btn={"btn btn-dark"}>Dine In</Button>
-                    </Link>
-                </Row>
-                <Row><a href={"https://www.yelp.com/order/v2/cart/954de1b27f3ee7705f2ca2d2c8336670"}>
+                </Col>
+                <Col >
+                <Link to={!state.userLoggedIn ? "/login": "/choice"}>
+                    <Button type={"button"} btn={"btn btn-dark"}>Dine In</Button>
+                </Link>    
+                </Col>
+                <Col>
+                <a href={"https://www.yelp.com/order/v2/cart/954de1b27f3ee7705f2ca2d2c8336670"}>
                         <Button type={"button"} btn={"btn btn-dark"}>Carry Out</Button>
                         </a>
-                </Row>
-            </Col>
+                </Col>
+            </Row>
+            
         </Container>
     </React.Fragment>);
 }
