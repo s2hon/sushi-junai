c7ae52591b2bd93a610d28c21952453c86476d27
Christi Kerr <christinakerr394@gmail.com>
2020-12-07 18:24:14 -0600

    styled the order summary page

--- client/src/components/OrderSummary/MenuCart.js
+++ client/src/components/OrderSummary/MenuCart.js
@@ -28,7 +28,8 @@ const MenuCart = (props) => {
         return (
             <>
                 <Container>
-                    <Table>
+                    <div className="menu-container">
+                    <Table bordered striped>
                         <thead>
                             <tr>
                                 <th> <h1>Item (0)</h1></th>
@@ -56,6 +57,7 @@ const MenuCart = (props) => {
                             </tbody>
                         </thead>
                     </Table>
+                    </div>
                 </Container>
             </>
         )
@@ -64,8 +66,10 @@ const MenuCart = (props) => {
     return (
         <>
             <Container>
+            <div className="menu-container">
                 <Row><h3 className="cartHeading">Order Summary ({currentQtyTotal})</h3></Row>
-                <Table>
+                <div className="menu-card">
+                <Table bordered striped>
                     <thead>
                         <tr>
                             <th> </th>
@@ -77,14 +81,15 @@ const MenuCart = (props) => {
                     </thead>
                     <tbody className="mainFont">
                         {menuCart.map((item, idx) => <MenuCartItem key={idx} item={item} incrementItem={props.incrementItem} decrementItem={props.decrementItem} removeItem={props.removeItem} />)}
-                        <tr>
-                            <Link
+                        </tbody>
+                        </Table>
+                        <Link
                                 to="/ayce"
                                 className={window.location.pathname === "/ayce" ? "nav-link active" : "nav-link"}
-                            ><Button type={"button"} btn={"btn btn1"}>BACK TO MENU</Button>
-                            
+                            ><Button type={"button"} className={"btn btn1"}><h5>BACK TO MENU</h5></Button>
                             </Link>
-                        </tr>
+                        <Table>
+                            <tbody>
                         <tr>
 
                             <th scope="row"></th>
@@ -95,6 +100,8 @@ const MenuCart = (props) => {
                         <TipCalculater total={cartCostTotal.toFixed(2)} />
                     </tbody>
                 </Table>
+                </div>
+                </div>
             </Container>
         </>
     );
