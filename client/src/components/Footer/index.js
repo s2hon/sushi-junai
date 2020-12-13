c4e78e256eb66cd426d0b3b0951f3495bccef2be
Michelle Moon <robbins.michelle@ymail.com>
2020-12-13 11:47:17 -0600

    footer wip

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -9,48 +9,35 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-var style = {
-  backgroundColor: "#F26430",
-  borderTop: "none",
-  textAlign: "center",
-  padding: "20px",
-  position: "fixed",
-  left: "0",
-  bottom: "0",
-  height: "60px",
-  width: "100%",
-  fontWeight: "bold",
-  color: "white",
-
-}
-
-const phantom = {
-display: 'block',
-padding: '20px',
-height: '60px',
-width: '100%',
-}
-function Footer() {
+function Footer(props){
   return (
-    <>
-    <footer className="footer"style={{color: "white"}}>
-      <div style={phantom} />
-      <p style={{textAlign: "center"}}>Connect with us!</p>
-              <div className="footer-social-container" style={{textAlign: "center"}}>
-                <a style={{color: "white"}}target={"_blank"} rel="noreferrer" href="https://www.facebook.com/sushijunai/"><FontAwesomeIcon icon={faFacebookF} size="3x" /></a>
-                <a style={{color: "white"}}target={"_blank"} rel="noreferrer" href="https://www.instagram.com/sushijunai/"><FontAwesomeIcon icon={faInstagram} size="3x" /></a>
-                <a style={{color: "white"}}target={"_blank"} rel="noreferrer" href="mailto:sushijunai1@gmail.com"><FontAwesomeIcon icon={faEnvelope} size="3x" /></a>
+    <footer className="wpo-site-footer">
+      <div className="wpo-upper-footer">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col col-lg-3 col-md-6 col-sm-12 col-12 text-center">
+              <p>Connect with us!</p>
+              <div className="footer-social-container">
+                <a target={"_blank"} rel="noreferrer" href="https://www.facebook.com/sushijunai/"><FontAwesomeIcon icon={faFacebookF} size="3x" /></a>
+                <a target={"_blank"} rel="noreferrer" href="https://www.instagram.com/sushijunai/"><FontAwesomeIcon icon={faInstagram} size="3x" /></a>
+                <a target={"_blank"} rel="noreferrer" href="mailto:sushijunai1@gmail.com"><FontAwesomeIcon icon={faEnvelope} size="3x" /></a>
               </div>
-              <div className="row">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wpo-lower-footer">
+        <div className="container">
+          <div className="row">
             <div className="col col-xs-12">
-              <a style={{color: "white"}} className="copyright" href="https://github.com/s2hon/sushi-junai">&copy;MIT <FontAwesomeIcon icon={faGithub} size="2x" /> Built with love
+              <a className="copyright" href="https://github.com/s2hon/sushi-junai">&copy;MIT <FontAwesomeIcon icon={faGithub} size="2x" /> Built with love
               </a>
-              </div>
-              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
-    </>
-  );
+  )
 }
-
 
 export default Footer;
