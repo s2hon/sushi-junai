98f30dbb90d43f01fd745be1b500725b2751d867
Christi Kerr <christinakerr394@gmail.com>
2020-12-13 16:48:12 -0600

    Broke drink menu up by subcategories

--- client/src/components/DrinkMenu/MenuList.js
+++ client/src/components/DrinkMenu/MenuList.js
@@ -6,85 +6,167 @@ import MenuItems from "./MenuItems";
 
 function Menu(props) {
     const drinkMenu = menu.filter(item => item.menu === "drinks");
-    const sake = drinkMenu.filter(item => item.category === "sake");
-    const wine = drinkMenu.filter(item => item.category === "wine");
+    const sake = drinkMenu.filter(item => item.subcategory === "sake");
+    const premiumSake = drinkMenu.filter(item => item.subcategory === "premium sake");
+    const flavoredSake = drinkMenu.filter(item => item.subcategory === "flavored sake");
+    const plumWine = drinkMenu.filter(item => item.category === "plum wine");
+    const whiteWine = drinkMenu.filter(item => item.subcategory === "white");
+    const sparklingWine = drinkMenu.filter(item => item.subcategory === "sparkling");
+    const redWine = drinkMenu.filter(item => item.subcategory === "red");
     const beer = drinkMenu.filter(item => item.category === "beer");
     const softDrinks = drinkMenu.filter(item => item.category === "soft drinks")
     return (
         <>
             <Container>
                 <div className="menu-container">
-                <Row>
-                    <h1>Drink Menu</h1><hr />
-                </Row>
-                <Row>
-                <div className="section-head"><h2 id="Sake">Sake</h2></div>
-                    <Container>
-                        {
-                            sake.map((item, idx) => {
-                                return (
-                                    <div className="card menu-card text-center">
-                                        <div className="card-body d-flex justify-content-between">
-                                            <MenuItems key={idx} name={item.name} price={item.price} description={item.description} subtitle={item.subtitle} size={item.size} smallPrice={item.smallPrice} largePrice={item.largePrice}>
-                                            </MenuItems>
+                    <Row>
+                        <h1>Drink Menu</h1><hr />
+                    </Row>
+                    <Row>
+                        <div className="section-head"><h2 id="Sake">Sake</h2></div>
+                        <Container>
+                            {
+                                sake.map((item, idx) => {
+                                    return (
+                                        <div className="card menu-card text-center">
+                                            <div className="card-body d-flex justify-content-between">
+                                                <MenuItems key={idx} name={item.name} price={item.price} description={item.description} subtitle={item.subtitle} size={item.size} smallPrice={item.smallPrice} largePrice={item.largePrice}>
+                                                </MenuItems>
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </Container>
-                </Row>
-                <Row>
-                <div className="section-head"><h2 id="Wine">Wine</h2></div>
-                    <Container>
-                        {
-                            wine.map((item, idx) => {
-                                return (
-                                    <div className="card menu-card text-center">
-                                        <div className="card-body d-flex justify-content-between">
-                                            <MenuItems key={idx} name={item.name} glassPrice={item.glassPrice} bottlePrice={item.bottlePrice} origin={item.origin} description={item.description} >
-                                            </MenuItems>
+                                    )
+                                })
+                            }
+
+                            <h4 className="subhead">Premium Sake</h4>
+                            {
+                                premiumSake.map((item, idx) => {
+                                    return (
+                                        <div className="card menu-card text-center">
+                                            <div className="card-body d-flex justify-content-between">
+                                                <MenuItems key={idx} name={item.name} price={item.price} description={item.description} subtitle={item.subtitle} size={item.size} smallPrice={item.smallPrice} largePrice={item.largePrice}>
+                                                </MenuItems>
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </Container>
-                </Row>
-                <Row>
-                <div className="section-head"><h2 id="Beer">Beer</h2></div>
-                    <Container>
-                        {
-                            beer.map((item, idx) => {
-                                return (
-                                    <div className="card menu-card text-center">
-                                        <div className="card-body d-flex justify-content-between">
-                                            <MenuItems key={idx} name={item.name} size={item.size} price={item.price} description={item.description} >
-                                            </MenuItems>
+                                    )
+                                })
+                            }
+                            <h4 className="subhead">Flavored Sake</h4>
+                            {
+                                flavoredSake.map((item, idx) => {
+                                    return (
+                                        <div className="card menu-card text-center">
+                                            <div className="card-body d-flex justify-content-between">
+                                                <MenuItems key={idx} name={item.name} price={item.price} description={item.description} subtitle={item.subtitle} size={item.size} smallPrice={item.smallPrice} largePrice={item.largePrice}>
+                                                </MenuItems>
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </Container>
-                </Row>
-                <Row>
-                <div className="section-head"><h2 id="Soft Drinks">Soft Drinks</h2></div>
-                    <Container>
-                        {
-                            softDrinks.map((item, idx) => {
-                                return (
-                                    <div className="card menu-card text-center">
-                                        <div className="card-body d-flex justify-content-between">
-                                            <MenuItems key={idx} name={item.name} price={item.price} drinks={item.drinks} description={item.description} >
-                                            </MenuItems>
+                                    )
+                                })
+                            }
+                        </Container>
+
+
+                    </Row>
+                    <Row>
+                        <div className="section-head"><h2 id="Wine">Plum Wine</h2></div>
+                        <Container>
+                            {
+                                plumWine.map((item, idx) => {
+                                    return (
+                                        <div className="card menu-card text-center">
+                                            <div className="card-body d-flex justify-content-between">
+                                                <MenuItems key={idx} name={item.name} price={item.price} description={item.description} subtitle={item.subtitle} size={item.size} smallPrice={item.smallPrice} largePrice={item.largePrice}>
+                                                </MenuItems>
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </Container>
-                </Row>
+                                    )
+                                })
+                            }
+                        </Container>
+                    </Row>
+                    <Row>
+                        <div className="section-head"><h2 id="Wine">Wine</h2></div>
+                        <Container>
+                            <h4 className="subhead">Sparkling</h4>
+                            {
+                                sparklingWine.map((item, idx) => {
+                                    return (
+                                        <div className="card menu-card text-center">
+                                            <div className="card-body d-flex justify-content-between">
+                                                <MenuItems key={idx} name={item.name} glassPrice={item.glassPrice} bottlePrice={item.bottlePrice} origin={item.origin} description={item.description} >
+                                                </MenuItems>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+
+
+                            <h4 className="subhead">White</h4>
+                            {
+                                whiteWine.map((item, idx) => {
+                                    return (
+                                        <div className="card menu-card text-center">
+                                            <div className="card-body d-flex justify-content-between">
+                                                <MenuItems key={idx} name={item.name} glassPrice={item.glassPrice} bottlePrice={item.bottlePrice} origin={item.origin} description={item.description} >
+                                                </MenuItems>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+
+                            <h4 className="subhead">Red</h4>
+                            {
+                                redWine.map((item, idx) => {
+                                    return (
+                                        <div className="card menu-card text-center">
+                                            <div className="card-body d-flex justify-content-between">
+                                                <MenuItems key={idx} name={item.name} glassPrice={item.glassPrice} bottlePrice={item.bottlePrice} origin={item.origin} description={item.description} >
+                                                </MenuItems>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+
+                        </Container>
+                    </Row>
+                    <Row>
+                        <div className="section-head"><h2 id="Beer">Beer</h2></div>
+                        <Container>
+                            {
+                                beer.map((item, idx) => {
+                                    return (
+                                        <div className="card menu-card text-center">
+                                            <div className="card-body d-flex justify-content-between">
+                                                <MenuItems key={idx} name={item.name} size={item.size} price={item.price} description={item.description} >
+                                                </MenuItems>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </Container>
+                    </Row>
+                    <Row>
+                        <div className="section-head"><h2 id="Soft Drinks">Soft Drinks</h2></div>
+                        <Container>
+                            {
+                                softDrinks.map((item, idx) => {
+                                    return (
+                                        <div className="card menu-card text-center">
+                                            <div className="card-body d-flex justify-content-between">
+                                                <MenuItems key={idx} name={item.name} price={item.price} drinks={item.drinks} description={item.description} >
+                                                </MenuItems>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </Container>
+                    </Row>
                 </div>
             </Container>
         </>
