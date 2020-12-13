36c9d78683c0a2fc7f53e13f6247606060a6c8ac
Christi Kerr <christinakerr394@gmail.com>
2020-12-13 16:54:43 -0600

    Added filters to see only drinks available at certain locations

--- client/src/components/DrinkMenu/MenuList.js
+++ client/src/components/DrinkMenu/MenuList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Container from "../Container";
 import Row from "../Row";
 import menu from "../../db/menu.json";
 import MenuItems from "./MenuItems";
 
 function Menu(props) {
-    const drinkMenu = menu.filter(item => item.menu === "drinks");
+    const [sj1, setsj1] = useState(false);
+    const [sj2, setsj2] = useState(false);
+
+    let drinkMenu = menu.filter(item => item.menu === "drinks");
+
+    if (sj1) {
+        drinkMenu = drinkMenu.filter(item => item.restaurant === "sj1" || item.restaurant === "both")
+    }
+
+    if (sj2) {
+        drinkMenu = drinkMenu.filter(item => item.restaurant === "sj2" || item.restaurant === "both")
+    }
+
     const sake = drinkMenu.filter(item => item.subcategory === "sake");
     const premiumSake = drinkMenu.filter(item => item.subcategory === "premium sake");
     const flavoredSake = drinkMenu.filter(item => item.subcategory === "flavored sake");
@@ -15,12 +27,30 @@ function Menu(props) {
     const redWine = drinkMenu.filter(item => item.subcategory === "red");
     const beer = drinkMenu.filter(item => item.category === "beer");
     const softDrinks = drinkMenu.filter(item => item.category === "soft drinks")
+
+    function handleInputChangeSJ1() {
+        setsj1(!sj1);
+    }
+
+    function handleInputChangeSJ2() {
+        setsj2(!sj2);
+    }
     return (
         <>
             <Container>
                 <div className="menu-container">
                     <Row>
                         <h1>Drink Menu</h1><hr />
+                    </Row>
+                    <Row className="sticky-top menuOptions">
+                        <span class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="sj1Checkbox" onChange={handleInputChangeSJ1} value="sj1" />
+                            <label class="form-check-label" for="sj1Checkbox">Sushi Junai 1 (Downtown) </label>
+                        </span>
+                        <span class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="sj2Checkbox" onChange={handleInputChangeSJ2} value="sj2" />
+                            <label class="form-check-label" for="sj2Checkbox">Sushi Junai 2 (North Austin) </label>
+                        </span>
                     </Row>
                     <Row>
                         <div className="section-head"><h2 id="Sake">Sake</h2></div>
