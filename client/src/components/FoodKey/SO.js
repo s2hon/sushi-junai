2c355b4f87f5f03d91eca0241f0ee844f728ebfd
Christi Kerr <christinakerr394@gmail.com>
2020-12-14 12:55:49 -0600

    Built sauce key components

---
+++ client/src/components/FoodKey/SO.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Row from "../Row";
+
+
+export default function SO () {
+    return (
+        <Row>
+        <h5>*S.O. sauce: Sweet mayo, masago, green onion, ponzu</h5>
+        </Row>
+    )
+}
---
+++ client/src/components/FoodKey/YumYum.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Row from "../Row";
+
+
+export default function YumYum () {
+    return (
+        <Row>
+        <h5>**Yum yum sauce: sweet mayo</h5>
+        </Row>
+    )
+}
