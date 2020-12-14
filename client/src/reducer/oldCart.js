7314286b9541b4a11e6fdb40223c819e7f3dc18c
Staci Shon <staci.shon@gmail.com>
2020-12-14 15:20:16 -0600

    merged with main branch

--- client/src/reducer/oldCart.js
+++ client/src/reducer/oldCart.js
@@ -6,6 +6,7 @@ const oldCart = (state=persistedState || [], action) => {
     switch (action.type) {
         case 'SAVE_PAST_CART':{
             const { menuCart } = action.payload
+            // const findItem = state.find( x => x.name == item.name)
             if (!state.length) {
                 state = menuCart
             }
