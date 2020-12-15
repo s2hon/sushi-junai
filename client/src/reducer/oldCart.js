2ef31f33ece5cd4c8ac4c0654e47426dc01c0522
Staci Shon <staci.shon@gmail.com>
2020-12-15 14:13:23 -0600

    order summary past orders finished

--- client/src/reducer/oldCart.js
+++ client/src/reducer/oldCart.js
@@ -1,4 +1,5 @@
 import {loadState} from "../localstorage";
+import menuCart from "./menuCart";
 
 const persistedState = loadState("oldCart");
 
@@ -6,14 +7,32 @@ const oldCart = (state=persistedState || [], action) => {
     switch (action.type) {
         case 'SAVE_PAST_CART':{
             const { menuCart } = action.payload
-            // const findItem = state.find( x => x.name == item.name)
             if (!state.length) {
                 state = menuCart
             }
             else {
-                return [...state, ...menuCart]
-            } 
-        }
+                const combined = [...state, ...menuCart]
+
+                const itemsObj = {}
+                combined.reduce(((key) => itemsObj[key] = itemsObj), {})
+                console.log(itemsObj)
+
+                for (let i = 0; i < combined.length; ++i) {
+                    const name = combined[i].name
+                    if (itemsObj[name] === undefined) {
+                        itemsObj[name] = combined[i]
+                    } else {
+                        const newQuantity = itemsObj[name].quantity + combined[i].quantity
+                        itemsObj[name].quantity = newQuantity
+                        console.log(itemsObj)
+                    }
+                }
+
+                state = Object.keys(itemsObj).map(key => {
+                    return itemsObj[key];
+                })
+            }
+        } 
         default:
             return state
     }
