f171f03b45e68307c4518914899bb90d348d4c7f
Staci Shon <staci.shon@gmail.com>
2020-12-15 16:08:22 -0600

    fixed navbar

--- client/src/pages/Alacarte.js
+++ client/src/pages/Alacarte.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ALaCarteMenu from "../components/ALaCarteMenu/MenuList"
 import Counter from "../components/Counter";
-import BackButton from "../components/BackButton";
 
 function Alacarte(props) {
     return (
         <>  
-            <BackButton />
             <Counter/>
             <ALaCarteMenu />
         </>
