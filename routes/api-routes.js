ad3d65184934c6e7d3c419d00cfe9e3084a3a7cf
aminadab.morales <aminadabm93@gmail.com>
2021-01-06 14:03:28 -0600

    changing api key name

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,11 @@
 
 const axios = require("axios");
 require('dotenv').config();
-const API_KEY = process.env.API_KEY;
+const REACT_APP_API_KEY = process.env.API_KEY;
 const BASEURL = "https://api.yelp.com/v3/businesses/uK3hclf6oje7rRAbUhPCLg/reviews";
 
 module.exports = function(app) {
+  console.log("the api key is "+ REACT_APP_API_KEY);
   app.get("/api/reviews", function(req, res) {    
     try {
       axios
@@ -12,7 +13,7 @@ module.exports = function(app) {
           BASEURL,
           {
             headers: {
-                Authorization: `Bearer ${API_KEY}`
+                Authorization: "Bearer "+REACT_APP_API_KEY
             }
           }
       ) 
