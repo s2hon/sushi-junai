19a4d602618e743283586c935735a89bb5dc85b7
Staci Shon <staci.shon@gmail.com>
2021-01-06 08:35:34 -0600

    removed all unnecessary files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,13 +2,7 @@
 const path = require("path");
 
 module.exports = function(app) {
-  app.use(express.static(path.join(__dirname, 'build')));
-
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-
-  app.use('/dinein', function(req, res) {
-      res.sendFile(path.join(__dirname, 'build', "dinein.html"))
+  app.use(function(req, res) {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"))
   })
 };
