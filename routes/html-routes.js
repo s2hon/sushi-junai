1279b3bf53c76db40f023e81d2abefcf197a9647
Staci Shon <staci.shon@gmail.com>
2021-01-06 12:25:05 -0600

    deleted redirects and toml

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 module.exports = function(app) {
-  app.use(function(req, res) {
-      res.sendFile(path.join(__dirname, "../client/build/index.html"))
-  })
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 };
