82c15098649cdb0cd419d73c844e49c5547959c5
Staci Shon <staci.shon@gmail.com>
2021-01-07 11:14:51 -0600

    removed app.use

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,9 +2,6 @@
 const path = require("path");
 
 module.exports = function(app) {
-
-  app.use(express.static(__dirname));
-
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
