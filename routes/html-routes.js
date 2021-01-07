2f62ebb3a9a0e4e7be5ab270a75f3d4f26346540
Staci Shon <staci.shon@gmail.com>
2021-01-07 00:38:13 -0600

    edited html to original file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"))
   })
 
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
+  // app.get('/*', function (req, res) {
+  //   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  // });
 };
