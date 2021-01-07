9c419bc077746e114b4c446354a481de627fbe82
Staci Shon <staci.shon@gmail.com>
2021-01-07 11:38:18 -0600

    merged with main

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {saveState} from "./localstorage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducer';
 import data from './db/menu.json';
+import Home from "./pages/Home";
+import Alacarte from "./pages/Alacarte";
+import AYCEMenu from "./pages/AYCEMenu";
+import OrderSummary from "./pages/OrderSummary";
+import Dinein from "./pages/Dinein";
+import Rules from "./pages/Rules";
+import Drinks from "./pages/Drinks";
+import LunchMenu from './pages/LunchMenu';
+import StaticAYCEMenu from './pages/StaticAYCEMenu';
+import Delivery from './pages/Delivery';
+import ToGo from './pages/ToGo';
+import Header from "./components/Header";
+import Footer from './components/Footer';
 import moment from "moment";
-import ScrollToTop from "./components/ScrollToTop";
+import ScrollToTop from "./components/ScrollToTop"
 
 const store = createStore(reducer);
 data.forEach((item) => store.dispatch({
@@ -28,7 +42,22 @@ function App() {
         <Provider store={store}>
             <Router>
                 <ScrollToTop>
-                    <AllRouter />
+                    <Header/>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/home" component={Home}/>
+                        <Route exact path="/alacarte" component={Alacarte}/>
+                        <Route exact path="/ayce" component={AYCEMenu}/>
+                        <Route exact path="/dinein" component={Dinein}/>
+                        <Route exact path="/ordersummary" component={OrderSummary}/>            
+                        <Route exact path="/rules" component={Rules}/>
+                        <Route exact path="/drinks" component={Drinks}/>
+                        <Route exact path="/lunch" component={LunchMenu}/>
+                        <Route exact path="/aycestatic" component={StaticAYCEMenu}/>
+                        <Route exact path="/togo" component={ToGo}/>
+                        <Route exact path="/delivery" component={Delivery}/>
+                    </Switch>
+                    <Footer />
                 </ScrollToTop>
             </Router>
         </Provider>
