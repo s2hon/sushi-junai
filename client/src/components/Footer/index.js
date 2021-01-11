a3b02191060b175b03348cb34b191ca249e30978
Staci Shon <staci.shon@gmail.com>
2021-01-11 02:52:17 -0600

    edited header top bar, footer and logo

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,17 +2,34 @@ import React from 'react';
 import './style.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faFacebookF,
   faGithub,
+  faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Footer(props){
   return (
     <footer className="wpo-site-footer">
+      <div className="wpo-upper-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col col-12 text-center">
+              <p>Connect with us!</p>
+              <div className="footer-social-container">
+                <a target={"_blank"} rel="noreferrer" href="https://www.facebook.com/sushijunai/"><FontAwesomeIcon icon={faFacebookF} size="3x" /></a>
+                <a target={"_blank"} rel="noreferrer" href="https://www.instagram.com/sushijunai/"><FontAwesomeIcon icon={faInstagram} size="3x" /></a>
+                <a target={"_blank"} rel="noreferrer" href="mailto:sushijunai1@gmail.com"><FontAwesomeIcon icon={faEnvelope} size="3x" /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="wpo-lower-footer">
         <div className="container">
           <div className="row">
-            <div className="col col-xs-12">
-              <a className="copyright" href="https://github.com/s2hon/sushi-junai">&copy;MIT <FontAwesomeIcon icon={faGithub} size="2x" /> Built with caffiene and love
+            <div className="col col-12">
+              <a className="copyright" target={"_blank"} rel="noreferrer" href="https://github.com/sushi-junai-atx/sushi-junai-atx-official">&copy;MIT <FontAwesomeIcon icon={faGithub} size="2x" /> Built with caffiene and love
               </a>
             </div>
           </div>
--- client/src/components/HeaderTopbar/index.js
+++ client/src/components/HeaderTopbar/index.js
@@ -1,12 +1,4 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { 
-    faEnvelope
-} from "@fortawesome/free-solid-svg-icons";
-import {
-    faFacebookF,
-    faInstagram
-} from "@fortawesome/free-brands-svg-icons";
 import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import Feedback from '../Feedback';
 import './style.css';
@@ -31,30 +23,18 @@ const HeaderTopbar = (props) => {
                     <div className="col col-md-6 col-sm-12 col-12">
                         <div className="contact-intro">
                             <ul>
-                                <li style={{padding: "6.5%"}}>
-                                    <Button className="btn btn2">
-                                        <a target="_blank" rel="noreferrer" href="https://www.instagram.com/sushijunai/">
-                                            <FontAwesomeIcon icon={faInstagram} />
-                                        </a>
-                                    </Button>
 
-                                    <Button className="btn btn2">
-                                        <a target="_blank" rel="noreferrer" href="https://www.facebook.com/sushijunai">
-                                            <FontAwesomeIcon icon={faFacebookF} />
-                                        </a>
-                                    </Button>
-                                </li>
-                                <li><a target="_blank" rel="noreferrer" href="mailto:sushijunai1@gmail.com"><FontAwesomeIcon icon={faEnvelope} className="fi" /> sushijunai1@gmail.com</a></li>
                             </ul>
                         </div>
                     </div>
                     <div className="col col-md-6 col-sm-12 col-12">
                         <div className="contact-info">
                             <ul>
-                                <li></li>
+                                <li>
+                                </li>
                                 <li>
                                     <div>
-                                        <Button color="danger" onClick={toggle}>{buttonLabel}Your Feedback Matters!</Button>
+                                        <Button style={{backgroundColor: "#E63736"}} onClick={toggle}>{buttonLabel}Your Feedback Matters!</Button>
                                         <Modal isOpen={modal} toggle={toggle} fade={false} className={className} style={{opacity:1}}>
                                             <ModalHeader toggle={toggle} close={closeBtn}>
                                                 <h3 className="section-head">Tell us what you think!</h3>
